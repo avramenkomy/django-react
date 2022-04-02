0ee4bf181a6e7dd9371cf78443d0a65c798e5f9b
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-02 17:55:04 +0300

    WIP: connect leads app to project

--- django_react/django_react/settings.py
+++ django_react/django_react/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'leads.apps.LeadsConfig',  # activate the new app
+    'leads.apps.LeadsConfig',  # activate the new app
 ]
 
 MIDDLEWARE = [
