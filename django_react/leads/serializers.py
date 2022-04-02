0de0f106a8446ba44c794014cab4862aa5aa58e2
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-02 20:36:46 +0300

    hotfix

--- django_react/leads/serializers.py
+++ django_react/leads/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Lead
 
 
-class LeadSerializers(serializers.ModelSerializer):
+class LeadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lead
         fields = ('id', 'name', 'email', 'message')
